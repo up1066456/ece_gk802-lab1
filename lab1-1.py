6c822a27627775bae9a78205742026905dd350bf
up1066456 <up1066456@upnet.gr>
2023-03-01 23:26:47 +0200

    Not Done

--- lab1-1.py
+++ lab1-1.py
@@ -3,8 +3,15 @@ import requests  # εισαγωγή της βιβλιοθήκης
 url = 'http://youtube.com/'  # προσδιορισμός του url
 
 with requests.get(url) as response:  # το αντικείμενο response
-    a=response.headers
-print("\nHeaders:",a)
-print("\nServer:", a["Server"])
-b=a["Set-Cookie"].split("; ")
-print("\nCookies",b)
+    headers=response.headers
+print("\nHeaders:",headers)
+print("\nServer:", headers["Server"])
+cookies=headers["Set-Cookie"].split("; ")
+print("\nCookies:",cookies)
+temp=[[],[],[]``]
+i=0
+for token in cookies:
+    temp[i].append(token)
+    if 'Expires' in token:
+        i+=1
+print("\n",temp)
