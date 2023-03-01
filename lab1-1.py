a7f845640d582cd52c9fd4936d2c4b65b5676084
up1066456 <up1066456@upnet.gr>
2023-03-01 23:38:08 +0200

    Tomorrow again

--- lab1-1.py
+++ lab1-1.py
@@ -4,14 +4,18 @@ url = 'http://youtube.com/'  # προσδιορισμός του url
 
 with requests.get(url) as response:  # το αντικείμενο response
     headers=response.headers
+    cookiesnames=response.cookies
 print("\nHeaders:",headers)
 print("\nServer:", headers["Server"])
 cookies=headers["Set-Cookie"].split("; ")
 print("\nCookies:",cookies)
-temp=[[],[],[]``]
+temp=[[]]
 i=0
 for token in cookies:
     temp[i].append(token)
     if 'Expires' in token:
+        temp.append([])
         i+=1
 print("\n",temp)
+print("\n",cookiesnames)
+
