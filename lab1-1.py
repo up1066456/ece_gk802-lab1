1648d97d21ad2abb55fd7817cd503c05d3b7cf0d
up1066456 <up1066456@upnet.gr>
2023-03-01 20:52:19 +0200

    Input

--- lab1-1.py
+++ lab1-1.py
@@ -6,13 +6,13 @@ def more(text):
     for line in text.split('\n'):
         print(line)
         count += 1
-        if count % 30 == 0:
+        if count % 5 == 0:
             reply = input('Show more (y/n)? ')
             if reply == 'n':
                 break
 
 
-url = str(input('url?'))  # προσδιορισμός του url
+url = input('url?')  # προσδιορισμός του url
 
 with requests.get(url) as response:  # το αντικείμενο response
     html = response.text
